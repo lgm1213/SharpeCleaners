a73887b9d0e889e50473720f7c859437c80d257d
LGM1213 <lgm1213@gmail.com>
2017-01-23 16:06:22 -0500

    new sessions controller and login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        flash[:success] = 'Welcome to Sharp Cleaners'
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
@@ -70,6 +71,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:username, :firstname, :lastname, :email, :address, :city, :state, :zipcode, :phone)
+      params.require(:user).permit(:username, :firstname, :lastname, :email, :address, :city, :state, :zipcode, :phone, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+	require 'bcrypt'
+
+	def password
+		@password ||= Password.new(password_hash)
+	end
+
+	def password=(new_password)
+	end
 end
